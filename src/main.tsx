b089edaa5d1416179da72a6aa8718441b4f6a227
Darkhand333 <harishdeivasagayam@gmail.com>
2025-09-29 12:17:18 +0530

    tests

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import OptimizedPortfolio from './pages/OptimizedPortfolio.tsx';
-// import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import OptimizedPortfolio from './pages/OptimizedPortfolio.tsx';
+import './index.css';
 
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <OptimizedPortfolio />
-//   </React.StrictMode>,
-// );
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <OptimizedPortfolio />
+  </React.StrictMode>,
+);
